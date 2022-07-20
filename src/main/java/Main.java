9e8592e0912e876fa36756d15c0f25ec45c966bc
seanjungmsba <seanjeong1996@gmail.com>
2022-07-20 12:32:16 -0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the series of numbers: ");
         String[] input = scanner.nextLine().split("\\s+");
         System.out.println(maxAbsoluteVal(input));
     }
@@ -15,6 +18,8 @@ public class Main {
      * @return the maximum integer absolute value in the array
      */
     public static int maxAbsoluteVal(String[] numbers) {
-
+        List<String> numbersList = Arrays.asList(numbers);
+        return numbersList.stream().mapToInt(strNum -> Integer.parseInt(strNum)).map(num -> Math.abs(num)).max().getAsInt();
     }
+    
 }
